aee27940f0c533c73afc793bd70ef4c95be503f4
colt <necromanga@gmail.com>
2016-04-12 11:04:18 +0200

    Aspect Ratios & Viewports.

--- core/src/com/colt/supermario/screens/ScreenPlay.java
+++ core/src/com/colt/supermario/screens/ScreenPlay.java
@@ -3,7 +3,12 @@ package com.colt.supermario.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.colt.supermario.Boot;
 
 /**
@@ -13,11 +18,15 @@ import com.colt.supermario.Boot;
 public class ScreenPlay implements Screen {
 
     private Boot game;
+    private OrthographicCamera camera;
+    private Viewport viewport;
     Texture texture;
 
     //Constructor.
     public ScreenPlay(Boot game) {
         this.game = game;
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(400, 400, camera); //Fit, Screen, Stretch.
         texture = new Texture("badlogic.jpg");
     }
 
@@ -30,6 +39,7 @@ public class ScreenPlay implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         game.batch.draw(texture, 10, 10); //Bottom left.
         game.batch.end();
@@ -37,7 +47,7 @@ public class ScreenPlay implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
